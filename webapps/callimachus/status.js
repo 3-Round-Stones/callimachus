60ae22f8d954730f0a6edf4881907f4974e62af4
james.leighnet <james.leighnet@5a417a11-4149-54be-8e14-488cf31dc158>
2010-07-29 14:26:16 +0000

    Fix js typo

--- webapps/callimachus/status.js
+++ webapps/callimachus/status.js
@@ -3,7 +3,7 @@
    Licensed under the Apache License, Version 2.0, http://www.apache.org/licenses/LICENSE-2.0
 */
 
-$(document).ready(initForms);
+$(document).ready(initStatus);
 
 var formRequestCount = 0;
 function initStatus() {

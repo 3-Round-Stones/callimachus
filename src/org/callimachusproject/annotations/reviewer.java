a46a6a548b45fe81ff207283c00f1782a4812682
james@leighnet.ca <james@leighnet.ca@5a417a11-4149-54be-8e14-488cf31dc158>
2010-12-09 20:53:02 +0000

    Change calli:reviewer to a class annotation

---
+++ src/org/callimachusproject/annotations/reviewer.java
@@ -0,0 +1,15 @@
+package org.callimachusproject.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.openrdf.repository.object.annotations.iri;
+
+@iri("http://callimachusproject.org/rdf/2009/framework#reviewer")
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.METHOD, ElementType.TYPE, ElementType.PARAMETER })
+public @interface reviewer {
+	String[] value();
+}

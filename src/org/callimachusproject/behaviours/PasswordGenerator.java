555eb79b0f243a6709de32711afbc63839b42e70
james@leighnet.ca <james@leighnet.ca@5a417a11-4149-54be-8e14-488cf31dc158>
2010-12-16 17:04:00 +0000

    Increase length of generated password

--- src/org/callimachusproject/behaviours/PasswordGenerator.java
+++ src/org/callimachusproject/behaviours/PasswordGenerator.java
@@ -28,12 +28,13 @@ import java.util.Random;
  */
 public class PasswordGenerator {
 	private static final Random rnd = new SecureRandom();
-	private static final int LENGTH_MIN = 6;
-	private static final int LENGTH_MAX = 10;
+	private static final int LENGTH_MIN = 8;
+	private static final int LENGTH_MAX = 16;
 	private static final char[] CHAR_POOL = { 'a', 'b', 'c', 'd', 'e', 'f',
-			'g', 'h', 'k', 'm', 'n', 'p', 'r', 'w', 'x', 'y', 'A', 'B', 'C',
-			'D', 'E', 'F', 'G', 'H', 'L', 'M', 'N', 'R', 'S', 'T', '2', '3',
-			'4', '5', '6', '7', '8', '!', '^', '*', '-', '_', '=', '.' };
+			'g', 'h', 'k', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'w', 'x', 'y',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'L', 'M', 'N', 'P', 'R',
+			'S', 'T', 'Z', '2', '3', '4', '5', '6', '7', '8', '9', '!', '^',
+			'*', '-', '_', '=', '.', '~', '[', ']', ';', ',' };
 
 	public String generatePassword() {
 		int length = LENGTH_MIN + rnd.nextInt(LENGTH_MAX - LENGTH_MIN + 1);

970af69eb4d1b158ca152af6992376c413f4a100
james@leighnet.ca <james@leighnet.ca@5a417a11-4149-54be-8e14-488cf31dc158>
2011-02-11 23:19:33 +0000

    Add numeric support in sorted and move menu file

--- webapps/callimachus/scripts/sorted.js
+++ webapps/callimachus/scripts/sorted.js
@@ -19,32 +19,31 @@ function sortElements(node) {
 	for (var i = 0; i < nodes.length; i++) {
 		list[i] = nodes[i];
 	}
+	var exclude = $(node).find(".sorted").find(".asc, .desc");
 	list.sort(function(a, b) {
 		if (a.nodeType < b.nodeType) return -1;
 		if (a.nodeType > b.nodeType) return 1;
 		if (a.nodeType != 1) return 0;
-		var a1 = $(a).find(".asc");
-		var a2 = $(b).find(".asc");
-		if (a1.length && a2.length) {
-			var s1 = a1[0].innerHTML.replace(/\s*<[^>]*>\s*/g, " ");
-			var s2 = a2[0].innerHTML.replace(/\s*<[^>]*>\s*/g, " ");
-			if (s1 < s2) return -1;
-			if (s1 > s2) return 1;
-		} else {
-			if (a1.length > a2.length) return -1;
-			if (a1.length < a2.length) return 1;
-		}
-		var d1 = $(a).find(".desc");
-		var d2 = $(b).find(".desc");
-		if (d1.length && d2.length) {
-			var s1 = d1[0].innerHTML.replace(/\s*<[^>]*>\s*/g, " ");
-			var s2 = d2[0].innerHTML.replace(/\s*<[^>]*>\s*/g, " ");
-			if (s1 > s2) return -1;
-			if (s1 < s2) return 1;
-		} else {
-			if (d1.length > d2.length) return -1;
-			if (d1.length < d2.length) return 1;
-		}
+		var a1 = $(a).find(".asc").not(exclude).text();
+		var a2 = $(b).find(".asc").not(exclude).text();
+		try {
+			var i1 = parseInt(a1);
+			var i2 = parseInt(a2);
+			if (i1 > i2) return 1;
+			if (i1 < i2) return -1;
+		} catch (e) {}
+		if (a1 > a2) return 1;
+		if (a1 < a2) return -1;
+		var d1 = $(a).find(".desc").not(exclude).text();
+		var d2 = $(b).find(".desc").not(exclude).text();
+		try {
+			var i1 = parseInt(d1);
+			var i2 = parseInt(d2);
+			if (i1 > i2) return -1;
+			if (i1 < i2) return 1;
+		} catch (e) {}
+		if (d1 > d2) return -1;
+		if (d1 < d2) return 1;
 		return 0;
 	});
 	for (var i = 0; i < nodes.length; i++) {

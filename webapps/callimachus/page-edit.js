02bd48695a68a890956c337da125a3cece1e5b0c
james@leighnet.ca <james@leighnet.ca@5a417a11-4149-54be-8e14-488cf31dc158>
2011-05-06 15:44:40 +0000

    Prompt for title if no header in file

--- webapps/callimachus/page-edit.js
+++ webapps/callimachus/page-edit.js
@@ -206,56 +206,66 @@ jQuery(function($){
 	$('#form').submit(function(event) {
 		var body = outlineProperties(jQuery.wymeditors(0).xhtml());
 		jQuery.getJSON('/callimachus/profile', function(json) {
-			var items = [];
-			$.each(json, function(key, val) {
-				if (key) {
-					items.push('xmlns:' + key + '="' + val + '"');
-				}
-			});
-			var header = page[1].replace(/<html\s+(xmlns[:\w]*="[^"]*"\s*)*/g, '<html ');
-			header = header.replace(/<html\s*/, '<html xmlns="http://www.w3.org/1999/xhtml" ' + items.join(' ') + ' ');
-			var m = body.match(/<h(\d)(\s*[^>]*>(?:[^<]|<[^h\/]|<\/[^h])*)<\/h\1>/);
-			if (m) {
-				header = header.replace(/<title\b[\s\S]*<\/title>/, '<title about="?this"' + m[2] + '</title>');
-				header = header.replace(/<title\b[^>]*\/>/, '<title about="?this"' + m[2] + '</title>');
-			}
-			m = body.match(/<h(\d)(\s*[^>]*)\/>/);
-			if (m) {
-				header = header.replace(/<title\b[\s\S]*<\/title>/, '<title about="?this"' + m[2] + '/>');
-				header = header.replace(/<title\b[^>]*\/>/, '<title about="?this"' + m[2] + '/>');
-			}
-			var html = header + body + page[3];
-			if (etag) {
-				jQuery.ajax({
-					type: "PUT", url: url,
-					beforeSend: function(xhr) {
-						xhr.setRequestHeader('If-Match', etag);
-					},
-					contentType: 'application/xhtml+xml',
-					data: html,
-					success: function() {
-						location.replace(url + "?view");
+			try {
+				var items = [];
+				$.each(json, function(key, val) {
+					if (key) {
+						items.push('xmlns:' + key + '="' + val + '"');
 					}
 				});
-			} else { // new page
-				if (!url) {
-					// no URL provided
-					var h1 = $(':header:first', $('#iframe')[0].contentWindow.document).text();
-					h1 = h1 ? h1.replace(/^\s+/, '').replace(/\s+$/, '').replace(/\s+/, ' ') : '';
-					url = 'http://localhost:8080/page/' + encodeURI(h1).replace(/%20/g,'+') + '.xhtml';
-					html = html.replace(/<title>[^<]*<\/title>/, '<title>' + h1 + '</title>');
+				var header = page[1].replace(/<html\s+(xmlns[:\w]*="[^"]*"\s*)*/g, '<html ');
+				header = header.replace(/<html\s*/, '<html xmlns="http://www.w3.org/1999/xhtml" ' + items.join(' ') + ' ');
+				var m = body.match(/<h(\d)(\s*[^>]*>(?:[^<]|<[^h\/]|<\/[^h])*)<\/h\1>/);
+				if (m) {
+					header = header.replace(/<title\b[\s\S]*<\/title>/, '<title about="?this"' + m[2] + '</title>');
+					header = header.replace(/<title\b[^>]*\/>/, '<title about="?this"' + m[2] + '</title>');
 				}
-				jQuery.ajax({
-					type: "POST", url: location.href,
-					beforeSend: function(xhr) {
-						xhr.setRequestHeader('Location', url);
-					},
-					contentType: 'application/xhtml+xml',
-					data: html,
-					success: function() {
-						location.replace(url + "?view");
+				m = body.match(/<h(\d)(\s*[^>]*)\/>/);
+				if (m) {
+					header = header.replace(/<title\b[\s\S]*<\/title>/, '<title about="?this"' + m[2] + '/>');
+					header = header.replace(/<title\b[^>]*\/>/, '<title about="?this"' + m[2] + '/>');
+				}
+				var html = header + body + page[3];
+				if (etag) {
+					jQuery.ajax({
+						type: "PUT", url: url,
+						beforeSend: function(xhr) {
+							xhr.setRequestHeader('If-Match', etag);
+						},
+						contentType: 'application/xhtml+xml',
+						data: html,
+						success: function() {
+							location.replace(url + "?view");
+						}
+					});
+				} else { // new page
+					if (!url) {
+						// no URL provided
+						var h1 = $(':header:first', $('#iframe')[0].contentWindow.document).text();
+						if (h1) {
+							h1 = h1.replace(/^\s+/, '').replace(/\s+$/, '').replace(/\s+/, ' ');
+						} else {
+							h1 = prompt('Please provide a page title', 'Enter Page Title Here');
+							if (!h1 || h1 == 'Enter Page Title Here') throw 'No page title provided';
+							html = html.replace(/<title>[^<]*<\/title>/, '<title>' + h1 + '</title>');
+							html = html.replace(/<title\b[^>]*\/>/, '<title>' + h1 + '</title>');
+						}
+						url = 'http://localhost:8080/page/' + encodeURI(h1).replace(/%20/g,'+') + '.xhtml';
 					}
-				});
+					jQuery.ajax({
+						type: "POST", url: location.href,
+						beforeSend: function(xhr) {
+							xhr.setRequestHeader('Location', url);
+						},
+						contentType: 'application/xhtml+xml',
+						data: html,
+						success: function() {
+							location.replace(url + "?view");
+						}
+					});
+				}
+			} catch (e) {
+				$('#form').trigger("calliError", e.description ? e.description : e);
 			}
 		});
 		event.preventDefault();

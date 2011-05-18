6af5d7f41a967b478914a5a6ab6b18919b93f444
steven.a.battle@googlemail.com <steven.a.battle@googlemail.com@5a417a11-4149-54be-8e14-488cf31dc158>
2011-05-18 09:27:47 +0000

    RDFaReader support for property expressions

--- src/org/callimachusproject/stream/BufferedXMLEventReader.java
+++ src/org/callimachusproject/stream/BufferedXMLEventReader.java
@@ -19,7 +19,6 @@ package org.callimachusproject.stream;
 
 import java.lang.reflect.UndeclaredThrowableException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 

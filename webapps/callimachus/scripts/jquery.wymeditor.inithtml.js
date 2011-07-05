31d2504105a71291919db3c91094b53cec2a1f76
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2011-07-05 19:57:03 +0000

    Fix js error

--- webapps/callimachus/scripts/jquery.wymeditor.inithtml.js
+++ webapps/callimachus/scripts/jquery.wymeditor.inithtml.js
@@ -5,13 +5,14 @@
  * The DOM iframe must have an id of 'wym-iframe'
  */
 WYMeditor.editor.prototype.initHtml = function(html) {
-	this._wym._html = html;
-	this.initIframe($('#wym-iframe')[0]);
+	var wym = this;
+	wym._wym._html = html;
+	wym.initIframe($('#wym-iframe')[0]);
 	$('#wym-iframe').trigger('load');
 	setTimeout(function(){
 		if (!$('body', $('#wym-iframe')[0].contentWindow.document).children().length) {
 			// IE need this called twice on first load
-			this.initIframe($('#wym-iframe')[0]);
+			wym.initIframe($('#wym-iframe')[0]);
 		}
 	}, 1000);
 };

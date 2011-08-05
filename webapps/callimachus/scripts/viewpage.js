0bd2d9f0e724667e312b1e36a70e3ff1546fd5b0
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2011-08-05 14:55:42 +0000

    Don't encode colon in goto URL

--- webapps/callimachus/scripts/viewpage.js
+++ webapps/callimachus/scripts/viewpage.js
@@ -53,7 +53,7 @@ window.calli.viewpage = function(uri) {
 	if (url.indexOf(prefix) == 0) {
 		url = url.substring(prefix.length - 1);
 	}
-	return prefix + 'callimachus/go?q=' + encodeURIComponent(url).replace(/%2F/g, '/');
+	return prefix + 'callimachus/go?q=' + encodeURIComponent(url).replace(/%2F/g, '/').replace(/%3A/g, ':');
 }
 
 window.calli.diverted = function(uri, query) {

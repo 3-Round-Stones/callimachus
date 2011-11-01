81cb76be0fafab7847ae05fbf9fba9c57c9f325a
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2011-11-01 15:20:52 +0000

    Show full local part in class index

--- src/org/callimachusproject/rdfa/RDFaReader.java
+++ src/org/callimachusproject/rdfa/RDFaReader.java
@@ -95,7 +95,8 @@ public class RDFaReader extends RDFEventReader {
 			+ "\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02FF\\u0370-\\u037D\\u037F-\\u1FFF\\u200C-\\u200D\\u2070-\\u218F\\u2C00-\\u2FEF\\u3001-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFFD"
 			+ "\\u00B7\\u0300-\\u036F\\u203F-\\u2040";
 	/** ^\{([NCNameChar]*):([NCNameChar]*?)\} */
-	public static final String PROPERTY_EXP_REGEX = "^\\{([" + NCNameChar+"]*):(["+NCNameChar+"]*?)\\}";
+	public static final String PROPERTY_EXP_REGEX = "\\{([" + NCNameChar
+			+ "]*):([" + NCNameChar + "]*?)\\}";
 	
 	// The TEXT_CONTENT signifier is required to distinguish content from property expressions
 	// where the same property is used for both in the same element. 

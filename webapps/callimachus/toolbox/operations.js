eb4dcf6170d1b2ebc75c52064a83c4112318f0d8
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2011-12-21 19:40:27 +0000

    Inline create operations to avoid HTTP dependency

--- webapps/callimachus/toolbox/operations.js
+++ webapps/callimachus/toolbox/operations.js
@@ -1,103 +1,11 @@
 // operations.js
 
-importClass(Packages.calli.Composite);
-importClass(Packages.calli.Creatable);
 importClass(Packages.calli.edit);
 importClass(Packages.calli.view);
-importClass(Packages.org.callimachusproject.io.MultipartParser);
 importClass(Packages.org.openrdf.http.object.exceptions.InternalServerError);
-importClass(Packages.org.openrdf.http.object.exceptions.BadRequest);
-importClass(Packages.org.openrdf.http.object.exceptions.Forbidden);
 
 function getViewPage() {
 	return findTemplate(this, view).calliConstruct(this, 'view');
-}
-
-function getCreatePage(msg) {
-	if (!this.calliCreate || this.calliCreate.isEmpty())
-		throw new InternalServerError("No create template");
-	if (this.calliCreate.size() != 1)
-		throw new InternalServerError("Multiple create templates");
-	return this.calliCreate.iterator().next().calliConstruct(null, 'create');
-}
-
-function getCreatorPage(msg) {
-	if (!(msg.create instanceof Creatable))
-		throw new BadRequest("Cannot create this class here: " + msg.create);
-	if (!msg.create.calliCreate || msg.create.calliCreate.isEmpty())
-		throw new InternalServerError("No create template");
-	if (msg.create.calliCreate.size() != 1)
-		throw new InternalServerError("Multiple create templates");
-	return msg.create.calliCreate.iterator().next().calliConstruct(null, 'create');
-}
-
-function postFactoryCreate(msg) {
-	if (!msg.location)
-		throw new BadRequest("No location provided");
-	if (!(msg.create instanceof Creatable))
-		throw new BadRequest("Cannot create: " + msg.create);
-	var creatorUri = this.toString();
-	var createdUri = msg.location.toString();
-	var dest = createdUri.substring(0, createdUri.lastIndexOf('/', createdUri.length - 2) + 1);
-	if (creatorUri != dest && creatorUri != dest.substring(0, dest.length - 1))
-		throw new BadRequest("Location URI must be nested");
-	if (createdUri.search(/[\s\#\?]/) >= 0 || createdUri.search(/^\w+:\/\/\S+/) != 0)
-		throw new BadRequest("Fragement or name resources are not supported");
-	var type = msg.type;
-	var bio = new java.io.BufferedInputStream(msg.body, 65536);
-	if (msg.type.indexOf("multipart/form-data") == 0) {
-		bio.mark(1024);
-		var parser = new MultipartParser(bio);
-		var file = parser.next();
-		var headers = parser.getHeaders();
-		var disposition = headers.get("content-disposition");
-		if (disposition && disposition.indexOf("filename=") >= 0) {
-			type = headers.get("content-type");
-			var xml = type == "text/xml" || type == "application/xml";
-			if (xml || type == "application/octet-stream" || type.indexOf("application/x-") == 0) {
-				var fileName = disposition.replace(/.*filename="/g, '').replace(/".*/g, '');
-				var mimetypes = new javax.activation.MimetypesFileTypeMap();
-				if (!"application/octet-stream".equals(mimetypes.getContentType(fileName))) {
-					type = mimetypes.getContentType(fileName);
-				}
-			}
-			bio = file;
-		} else { // not a file upload
-			bio.reset();
-		}
-	}
-	var construct = msg.create.LookupConstructor(type);
-	var iter = this.FindCreator(msg.location).iterator();
-	while (iter.hasNext()) {
-		var user = iter.next();
-		if (!construct.calliIsAuthorized(user, "POST", "create"))
-			throw new Forbidden("You are not permitted to create " + construct + " resources");
-	}
-	var newCopy = construct.PostCreate(bio, msg.location, type);
-	newCopy.calliEditor.addAll(this.FindContributor(newCopy));
-	newCopy.calliReader.addAll(this.SelectAllReaders());
-	newCopy.calliEditor.addAll(this.SelectAllEditors());
-	newCopy.calliAdministrator.addAll(this.SelectAllAdministrators());
-	construct.touchRevision(); // Update class index
-	if (msg.intermediate) {
-		var revision = this.auditRevision;
-		this.calliHasComponent.add(newCopy);
-		this.auditRevision = revision; // restore the previous revision
-	} else {
-		this.calliHasComponent.add(newCopy);
-	}
-	return newCopy;
-}
-
-function postCreate(msg) {
-	var template = this.calliCreate.iterator().next();
-	if (!template)
-		throw new InternalServerError("Cannot create " + this.toString() + " with " + msg.type);
-	if (msg.type != "application/rdf+xml" && msg.type.indexOf("application/rdf+xml;") != 0)
-		throw new BadRequest("File format is not recognized: " + msg.type);
-	var newCopy = template.calliCreateResource(msg.body, this.toString(), msg.location);
-	newCopy = newCopy.objectConnection.addDesignation(newCopy, this.toString());
-	return newCopy;
 }
 
 function getEditPage() {

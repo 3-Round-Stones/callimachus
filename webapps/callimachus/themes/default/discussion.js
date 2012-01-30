a41a085ff2417cdd0f11a1b631465d16bb400a1e
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2012-01-30 21:15:01 +0000

    Fix # of posts in discussion tab

--- webapps/callimachus/themes/default/discussion.js
+++ webapps/callimachus/themes/default/discussion.js
@@ -2,36 +2,34 @@
 
 (function($, jQuery){
 
-function getPageLocationURL() {
-	// window.location.href needlessly decodes URI-encoded characters in the URI path
-	// https://bugs.webkit.org/show_bug.cgi?id=30225
-	var path = location.pathname;
-	if (path.match(/#/))
-		return location.href.replace(path, path.replace('#', '%23'));
-	return location.href;
-}
-
+	function getPageLocationURL() {
+		// window.location.href needlessly decodes URI-encoded characters in the URI path
+		// https://bugs.webkit.org/show_bug.cgi?id=30225
+		var path = location.pathname;
+		if (path.match(/#/))
+			return location.href.replace(path, path.replace('#', '%23'));
+		return location.href;
+	}
 	function checkTab() {
-		$("#discussion-tab:visible").each(function() {
+		if (location.search == '?discussion') {
+			var url = getPageLocationURL();
+			var posts = $(".comment").parent().find("time").map(function(){ return $(this).attr("content"); });
+			try {
+				if (window.localStorage && posts.length) {
+					localStorage.setItem(url, posts.get().join(','));
+				}
+			} catch (e) { }
+		}
+		$("#access a[href='?discussion']:visible").each(function() {
 			var tab = $(this);
 			var url = this.href;
-			if (url) {
-				jQuery.ajax({ type: 'GET', url: url,
-					success: function(doc) { handleDiscussion(tab, url, doc); }
-				});
-			} else {
-				var url = getPageLocationURL();
-				var posts = $(".comment").parent().find(".datetime-locale").map(function(){ return $(this).attr("content"); });
-				try {
-					if (window.localStorage && posts.length) {
-						localStorage.setItem(url, posts.get().join(','));
-					}
-				} catch (e) { }
-			}
+			jQuery.ajax({ type: 'GET', url: url,
+				success: function(doc) { handleDiscussion(tab, url, doc); }
+			});
 		});
 	}
 	function handleDiscussion(tab, url, data) {
-		var posts = $(".comment", data).parent().find(".datetime-locale").map(function(){ return $(this).text(); });
+		var posts = $(".comment", data).parent().find("time").map(function(){ return $(this).text(); });
 		if (posts.length) {
 			if (window.localStorage && localStorage.getItem(url)) {
 				var seen = localStorage.getItem(url).split(',');

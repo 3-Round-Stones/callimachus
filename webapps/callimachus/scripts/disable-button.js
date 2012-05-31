d31cd8c19ec3489bb5c3937338427c85a2680ab3
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2012-05-31 18:39:07 +0000

    Add .btn css class to HTML buttons

--- webapps/callimachus/scripts/disable-button.js
+++ webapps/callimachus/scripts/disable-button.js
@@ -17,11 +17,14 @@ function flashButton(event) {
     setTimeout(function() {
         if (!button.attr('disabled')) {
             button.attr('disabled', 'disabled');
+            button.addClass("disabbled");
             setTimeout(function() {
                 button.removeAttr('disabled');
+                button.removeClass("disabbled");
             }, 5000);
             button.focus(function() {
                 button.removeAttr('disabled');
+                button.removeClass("disabbled");
                 return true;
             });
         }

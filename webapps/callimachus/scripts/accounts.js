2c7d3995c2c87fbfa5a7f1fa3f07dfda4239e7ee
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2012-08-30 15:12:34 +0000

    Use a login page instead of a rolldown login form

--- webapps/callimachus/scripts/accounts.js
+++ webapps/callimachus/scripts/accounts.js
@@ -7,12 +7,12 @@ if (!window.calli) {
 }
 window.calli.getUserIri = function() {
     var iri = null;
-    if (window.sessionStorage) {
-        iri = sessionStorage.getItem('UserIri');
+    if (window.localStorage) {
+        iri = localStorage.getItem('UserIri');
         if (iri)
             return iri;
     }
-    jQuery.ajax({ url: "/?login", async: false,
+    jQuery.ajax({ url: "/?profile", async: false,
         beforeSend: withCredentials,
         success: function(doc) {
             iri = /resource="([^" >]*)"/i.exec(doc)[1];
@@ -21,230 +21,84 @@ window.calli.getUserIri = function() {
     return iri;
 };
 
+var userName = null;
+
 $(document).bind("calliLogin", function(event) {
-    // remove bogus data
-    if (window.sessionStorage) {
-        sessionStorage.removeItem('Name');
-        sessionStorage.removeItem('UserIri');
-        localStorage.removeItem('Authorization');
-    }
-    var options = {type: "GET", url: "/?login",
-        success: function(doc) {
-            var iri = /resource="([^" >]*)"/i.exec(doc);
-            if (iri) {
-                try {
-                    if (window.sessionStorage) {
-                        // now logged in
-                        sessionStorage.setItem("UserIri", iri[1]);
-                    }
-                } catch (e) { }
-            }
-            var title = /<(?:\w*:)?title[^>]*>([^<]*)<\/(?:\w*:)?title>/i.exec(doc);
-            if (title) {
-                try {
-                    if (window.sessionStorage) {
-                        // now logged in
-                        sessionStorage.setItem("Name", title[1]);
-                        localStorage.setItem("Name", title[1]);
-                    }
-                } catch (e) { }
-                document.documentElement.className += ' login';
-                var e = jQuery.Event("calliLoggedIn");
-                e.title = title[1];
-                $(document).ready(function() {
-                    $(document.documentElement).removeClass('logout');
-                    $(document).trigger(e);
-                });
-            }
-            if (window.localStorage && event.username) {
-                var auth = "User " + event.username;
-                if (event.remember) {
-                    auth = event.username + ":" + event.password;
-                    if (window.btoa) {
-                        auth = "Basic " + window.btoa(auth);
-                    } else {
-                        auth = "Credentials " + auth;
-                    }
-                }
-                try {
-                    localStorage.setItem('Authorization', auth);
-                } catch (e) { }
-            }
-            if (!event.isDefaultPrevented() && event.location) {
-                location = event.location;
-            } else if (!event.isDefaultPrevented() && event.location) {
-                location.reload(false);
-            }
-        }
-    };
-    if (event.username) {
-        options.username = event.username;
-    }
-    if (event.password) {
-        options.password = event.password;
-    } else {
-        options.beforeSend = withCredentials;
-    }
-    try {
-        jQuery.ajax(options);
-    } catch (e) {
-        // Opera don't support spaces in ajax passwords
-        if (options.password) {
-            delete options.password;
-            jQuery.ajax(options);
-        }
-    }
+    var return_to = window.location.href;
+    window.location = "/?login&return_to=" + encodeURIComponent(return_to);
+    event.preventDefault();
+});
+
+$(document).bind("calliLoggedIn", function(event) {
+    document.documentElement.className += ' login';
+    $(document.documentElement).removeClass('logout');
+    userName = event.title;
 });
 
 $(document).bind("calliLogout", function(event) {
-    if (!window.sessionStorage || sessionStorage.getItem('Name')) {
-        jQuery.ajax({ type: 'GET', url: "/?logout",
-            username: 'logout', password: 'please',
-            success: function(data) {
-                location = "/";
+    jQuery.ajax({ type: 'GET', url: "/?logout",
+        username: 'logout', password: 'please',
+        success: function(data) {
+            if (window.localStorage) {
+                localStorage.removeItem('UserName');
+                localStorage.removeItem('UserIri');
             }
-        });
-    }
-    if (window.sessionStorage) {
-        sessionStorage.removeItem('Name');
-        localStorage.removeItem('Authorization');
-        localStorage.removeItem('NotLoginCount');
-        localStorage.removeItem('LoginCount');
-        localStorage.removeItem('Name');
-    }
+            $(document).trigger("calliLoggedOut");
+            window.location = "/";
+        }
+    });
+    event.preventDefault();
+});
+
+$(document).bind("calliLoggedOut", function(event) {
+    document.documentElement.className += ' logout';
+    $(document.documentElement).removeClass('login');
+    userName = null;
 });
 
 if (window.localStorage) {
-    // IE8 doesn't support event.key
-    var oldName = localStorage.getItem('Name');
-    var oldNotLoginCount = localStorage.getItem('NotLoginCount');
-    var oldLoginCount = localStorage.getItem('LoginCount');
-
-    var currently = '';
-
-    $(document).bind("calliLoggedIn", function(event) {
-        currently = event.title;
-    });
-
-    $(document).bind("calliLoggedOut", function() {
-        currently = '';
-    });
-
-    var handleStorageEvent = function(e) {
-        var newName = localStorage.getItem('Name');
-        var newNotLoginCount = localStorage.getItem('NotLoginCount');
-        var newLoginCount = localStorage.getItem('LoginCount');
-        if (newName != oldName) {
-            oldName = newName;
-            if (currently && !newName) {
+    var storageChanged = function() {
+        var newName = localStorage.getItem('UserName');
+        if (newName != userName) {
+            userName = newName;
+            if (!newName) {
                 // now logged out
-                document.documentElement.className += ' logout';
-                sessionStorage.removeItem('Name');
                 $(document).ready(function() {
-                    $(document.documentElement).removeClass('login');
                     $(document).trigger("calliLoggedOut");
                 });
-                location = "/";
-            } else if (!currently && newName) {
+            } else if (newName) {
                 // now logged in
-                sessionStorage.setItem("Name", newName);
-                document.documentElement.className += ' login';
                 var e = jQuery.Event("calliLoggedIn");
                 e.title = newName;
                 $(document).ready(function() {
-                    $(document.documentElement).removeClass('logout');
                     $(document).trigger(e);
                 });
             }
         }
-        if (newNotLoginCount != oldNotLoginCount) {
-            oldNotLoginCount = newNotLoginCount;
-            if (currently && newNotLoginCount) {
-                // a window is not sure if we are logged in
-                localStorage.setItem("Name", currently);
-                var count = localStorage.getItem('LoginCount');
-                localStorage.setItem('LoginCount', count ? parseInt(count) + 1 : 1);
-            } 
-        }
-        if (newLoginCount != oldLoginCount) {
-            oldLoginCount = newLoginCount;
-            if (!currently && newLoginCount) {
-                // another window says we are logged in
-                var value = localStorage.getItem("Name");
-                sessionStorage.setItem("Name", value);
-                document.documentElement.className += ' login';
-                var e = jQuery.Event("calliLoggedIn");
-                e.title = value;
-                $(document).ready(function() {
-                    $(document.documentElement).removeClass('logout');
-                    $(document).trigger(e);
-                });
-            } 
-        }
         return true;
     };
-    $(window).bind('storage', handleStorageEvent);
-    $(document).bind('storage', handleStorageEvent); // IE
+    $(window).bind('storage', storageChanged);
+    $(document).bind('storage', storageChanged); // IE
 }
 
-if (window.sessionStorage && sessionStorage.getItem("Name")) {
-    // logged in already
-    document.documentElement.className += ' login';
-    var name = sessionStorage.getItem("Name");
+if (window.localStorage && localStorage.getItem("UserName")) {
+    // logged in already or hasn't logged out yet
+    var name = localStorage.getItem("UserName");
     var e = jQuery.Event("calliLoggedIn");
     e.title = name;
     $(document).ready(function() {
         $(document).trigger(e);
     });
 } else {
-    document.documentElement.className += ' logout';
     $(document).ready(function() {
         $(document).trigger("calliLoggedOut");
     });
-    if (window.localStorage && localStorage.getItem('Authorization')) {
-        // was logged in previously
-        var auth = localStorage.getItem('Authorization');
-        if (auth && auth.indexOf("Basic ") == 0) {
-            // remember me Firefox, Chrome, Safari
-            var up = window.atob(auth.substring("Basic ".length));
-            var event = jQuery.Event("calliLogin");
-            event.preventDefault(); // don't reload page
-            event.username = up.substring(0, up.indexOf(':'));
-            event.password = up.substring(up.indexOf(':') + 1);
-            event.remember = true;
-            $(document).trigger(event);
-            return;
-        } else if (auth && auth.indexOf("Credentials ") == 0) {
-            // remember me IE
-            var up = auth.substring("Credentials ".length);
-            var event = jQuery.Event("calliLogin");
-            event.preventDefault(); // don't reload page
-            event.username = up.substring(0, up.indexOf(':'));
-            event.password = up.substring(up.indexOf(':') + 1);
-            event.remember = true;
-            $(document).trigger(event);
-            return;
-        } else if (localStorage.getItem('Name')) {
-            // was logged in before, prompt to log back in
-            try {
-                var count = localStorage.getItem('NotLoginCount');
-                localStorage.setItem('NotLoginCount', count ? parseInt(count) + 1 : 1);
-            } catch (e) { }
-            setTimeout(function() {
-                $(document).ready(function() {
-                    $(document).trigger("calliLogInPrompt");
-                });
-            }, 0); // check if already logged in
-        }
-    }
     // hasn't logged in using the login form; is this page protected?
     var xhr = jQuery.ajax({type: 'GET', url: calli.getPageUrl(),
         beforeSend: withCredentials,
         success: function() {
-            if (xhr.getResponseHeader("Authentication-Info")) { 
-                var event = jQuery.Event("calliLogin");
-                event.preventDefault(); // don't reload page
-                $(document).trigger(event);
+            if (xhr.getResponseHeader("Authentication-Info")) {
+                nowLoggedIn();
             } else if (!xhr.getAllResponseHeaders()) { // Opera sends empty response; try again w/o cache
                 xhr = jQuery.ajax({type: 'GET', url: calli.getPageUrl(),
                     beforeSend: function(xhr) {
@@ -253,9 +107,7 @@ if (window.sessionStorage && sessionStorage.getItem("Name")) {
                     },
                     success: function() {
                         if (xhr.getResponseHeader("Authentication-Info")) {
-                            var event = jQuery.Event("calliLogin");
-                            event.preventDefault(); // don't reload page
-                            $(document).trigger(event);
+                            nowLoggedIn();
                         }
                     }
                 });
@@ -263,6 +115,28 @@ if (window.sessionStorage && sessionStorage.getItem("Name")) {
         }
     });
 }
+
+function nowLoggedIn() {
+    jQuery.ajax({ url: "/?profile",
+        beforeSend: withCredentials,
+        success: function(doc) {
+            var iri = /resource="([^" >]*)"/i.exec(doc);
+            var title = /<(?:\w*:)?title[^>]*>([^<]*)<\/(?:\w*:)?title>/i.exec(doc);
+            if (iri && title) {
+                if (window.localStorage) {
+                    // now logged in
+                    localStorage.setItem("UserName", title[1]);
+                    localStorage.setItem("UserIri", iri[1]);
+                }
+                var e = jQuery.Event("calliLoggedIn");
+                e.title = title[1];
+                $(document).ready(function() {
+                    $(document).trigger(e);
+                });
+            }
+        }
+    });
+};
 
 function withCredentials(req) {
     try {
--- webapps/callimachus/scripts/login.js
+++ webapps/callimachus/scripts/login.js
@@ -3,135 +3,36 @@
 (function($, jQuery){
 
 $(document).ready(function() {
-    $("#logout-link").click(function(event) {
-        $(document).trigger(jQuery.Event("calliLogout"));
-        if (event.preventDefault) {
+    $("#login-link").click(function(event) {
+        var login = jQuery.Event("calliLogin");
+        $(this).trigger(login);
+        if (login.isDefaultPrevented()) {
             event.preventDefault();
+            return false;
         }
-        return false;
+        return true;
     });
 });
-
-var loginForm;
 
 $(document).bind("calliLoggedIn", function(event) {
     $(document).ready(function() {
-        var title = event.title;
-        $("#profile-link").text(title);
-        if (loginForm) {
-            $(loginForm).remove();
-            $("#login-form").remove();
-            loginForm = null;
+        if (event.title) {
+            $("#profile-link").text(event.title);
         }
     });
 });
 
-$(document).bind("calliLoggedOut", function(event) {
-    $(document).ready(function() {
-        if (!loginForm) {
-            loginForm = createLoginButton();
+$(document).ready(function() {
+    $("#logout-link").click(function(event) {
+        var logout = jQuery.Event("calliLogout");
+        $(this).trigger(logout);
+        if (logout.isDefaultPrevented()) {
+            event.preventDefault();
+            return false;
         }
+        return true;
     });
 });
-
-$(document).bind("calliLogInPrompt", function() {
-    $(document).ready(function() {
-        if (loginForm) {
-            login.call(loginForm);
-        }
-    });
-});
-
-function createLoginButton() {
-    if ($('#login').length) {
-        var loginForm = $('<form />')[0];
-        $(loginForm).attr('action', $('#profile-link')[0].getAttribute('href'));
-        $(loginForm).css('display', "inline-block");
-        var button = $('<button type="submit" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary" style="margin:0px" />');
-        var icon = $('<span class="ui-button-icon-primary ui-icon ui-icon-circle-arrow-s" style="position:absolute;top:50%;margin-top:-8px;height:16px;width:16px;"> </span>');
-        var text = $('<span class="ui-button-text" style="padding-left: 22px;">Login</span>');
-        button.append(icon);
-        button.append(text);
-        $(loginForm).append(button);
-        $('#login').append(loginForm);
-        $(loginForm).submit(login);
-        return loginForm;
-    }
-    return null;
-}
-
-function login(event) {
-    var link = $(this);
-    if (link.find('button').length) {
-        link = link.find('button');
-    }
-    if ($("#login-form").size() && $("#login-form").css('height') != '0px' ) {
-        $("#login-form").css('height', '0px').css('opacity', '0');
-        link.css('margin-left', null);
-        link.css('margin-bottom', null);
-        link.css('border-bottom-width', null);
-        link.css('padding-bottom', null);
-        link.css('top', null);
-        $(document).trigger("calliLogout");
-    } else {
-        link.css('margin-left', '0px');
-        link.css('margin-bottom', '0px');
-        link.css('padding-bottom', '0.4em');
-        link.css('border-bottom-width', '0px');
-        link.css('top', '1px');
-        function showForm(form) {
-            form.css('opacity', '1').css('height', form.children('div').outerHeight() + 'px');
-            setTimeout(function(){form.css('filter','none')}, 1000); // workaround for IE8 transition polyfill
-            $("input:first", form).focus();
-        }
-        if ($("#login-form").length) {
-            showForm($("#login-form"));
-        } else {
-            jQuery.ajax({ type: 'GET', url: calli.getCallimachusUrl("pages/login.html"),
-                success: function(data) {
-                    if ($(document.documentElement).is(".logout")) {
-                        var form = $(data).find("#login-form").andSelf().filter("#login-form");
-                        form.css('position', "absolute");
-                        form.css('top', link.offset().top + link.outerHeight(false));
-                        var clientWidth = document.documentElement.clientWidth;
-                        if (clientWidth / 2 < link.offset().left) {
-                            form.css('right', clientWidth - link.offset().left - link.outerWidth(false));
-                        } else {
-                            form.css('left', link.offset().left);
-                        }
-                        form.css('overflow', "hidden");
-                        form.css('padding', "0");
-                        form.css('height', "0px").css('opacity', '0');
-                        form.css('-webkit-transition', 'height 1s, opacity 1s');
-                        form.css('-moz-transition', 'height 1s, opacity 1s');
-                        form.css('transition', 'height 1s, opacity 1s');
-                        $('body').append(form);
-                        form.submit(submitLoginForm);
-                        setTimeout(function(){showForm(form)});
-                    }
-                }
-            })
-        }
-    }
-    if (event && event.preventDefault) {
-        event.preventDefault();
-    }
-    return false;
-}
-
-function submitLoginForm(event) {
-    try {
-        var event = jQuery.Event("calliLogin");
-        event.preventDefault();
-        event.username = this.elements['username'].value;
-        event.password = this.elements['password'].value;
-        event.remember = this.elements['remember'].checked;
-        $(document).trigger(event);
-    } catch (e) {
-        throw calli.error(e);
-    }
-    return false;
-}
 
 })(jQuery, jQuery);
 

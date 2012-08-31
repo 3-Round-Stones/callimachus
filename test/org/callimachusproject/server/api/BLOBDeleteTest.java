b276fabafe82e6288f3019dda15d62662db54a07
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2012-08-31 19:42:18 +0000

    Remove DOCTYP from xhtml page

--- test/org/callimachusproject/server/api/BLOBDeleteTest.java
+++ test/org/callimachusproject/server/api/BLOBDeleteTest.java
@@ -39,8 +39,6 @@ public class BLOBDeleteTest extends TestCase {
 			});
         	
         	put("page", new String[] { "page.xhtml", "application/xhtml+xml",
-							"<?xml version=\"1.0\" encoding=\"iso-8859-1\"?> <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \n " +
-							"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" +
 							"<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\"> \n" +
 							"<head> <title> Wikipedia </title> </head> \n" +
 							"<body> <p> Wikipedia is a great website. </p> </body> </html>" 

98695d8d4af521c98d76c856f2b740b11a899407
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2012-09-27 14:29:22 +0000

    Provide compatibility with 0.17 themes

--- webapps/callimachus/scripts/login.js
+++ webapps/callimachus/scripts/login.js
@@ -3,36 +3,64 @@
 (function($, jQuery){
 
 $(document).ready(function() {
-    $("#login-link").click(function(event) {
-        var login = jQuery.Event("calliLogin");
-        $(this).trigger(login);
-        if (login.isDefaultPrevented()) {
+    $("#logout-link").click(function(event) {
+        $(document).trigger(jQuery.Event("calliLogout"));
+        if (event.preventDefault) {
             event.preventDefault();
-            return false;
         }
-        return true;
+        return false;
     });
 });
+
+var loginForm;
 
 $(document).bind("calliLoggedIn", function(event) {
     $(document).ready(function() {
-        if (event.title) {
-            $("#profile-link").text(event.title);
+        var title = event.title;
+        $("#profile-link").text(title);
+        if (loginForm) {
+            $(loginForm).remove();
+            $("#login-form").remove();
+            loginForm = null;
         }
     });
 });
 
-$(document).ready(function() {
-    $("#logout-link").click(function(event) {
-        var logout = jQuery.Event("calliLogout");
-        $(this).trigger(logout);
-        if (logout.isDefaultPrevented()) {
-            event.preventDefault();
-            return false;
+$(document).bind("calliLoggedOut", function(event) {
+    $(document).ready(function() {
+        if (!loginForm) {
+            loginForm = createLoginButton();
         }
-        return true;
     });
 });
+
+function createLoginButton() {
+    if ($('#login').length) {
+        var loginForm = $('<form />')[0];
+        $(loginForm).attr('action', $('#profile-link')[0].getAttribute('href'));
+        $(loginForm).css('display', "inline-block");
+        var button = $('<button type="submit" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary" style="margin:0px" />');
+        var icon = $('<span class="ui-button-icon-primary ui-icon ui-icon-circle-arrow-s" style="position:absolute;top:50%;margin-top:-8px;height:16px;width:16px;"> </span>');
+        var text = $('<span class="ui-button-text" style="padding-left: 22px;">Login</span>');
+        button.append(icon);
+        button.append(text);
+        $(loginForm).append(button);
+        $('#login').append(loginForm);
+        $(loginForm).submit(login);
+        return loginForm;
+    }
+    return null;
+}
+
+function login(event) {
+    try {
+        event.preventDefault();
+        $(document).trigger("calliLogin");
+    } catch (e) {
+        throw calli.error(e);
+    }
+    return false;
+}
 
 })(jQuery, jQuery);
 

d666689c453aa456737b07eed98058ae8377d3a1
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2013-02-05 19:27:42 +0000

    Don't advertise iframe content size anymore (isn't used)

--- webapp/scripts/iframe.js
+++ webapp/scripts/iframe.js
@@ -1,34 +1,6 @@
 // iframe.js
 
 (function($) {
-
-    function checkWindowSize() {
-        var innerHeight = window.innerHeight || document.documentElement.clientHeight;
-        if (innerHeight < document.height) {
-            parent.postMessage('PUT height\n\n' + document.height, '*');
-        } else {
-            var scrollHeight = document.height;
-            $('#content').add($('#content').parents()).each(function(){
-                scrollHeight += this.scrollHeight - this.clientHeight;
-            });
-            if (scrollHeight > innerHeight) {
-                parent.postMessage('PUT height\n\n' + scrollHeight, '*');
-            }
-        }
-        var clientWidth = document.documentElement.clientWidth;
-        if (clientWidth < document.documentElement.scrollWidth) {
-            parent.postMessage('PUT width\n\n' + document.documentElement.scrollWidth, '*');
-        } else {
-            var scrollWidth = document.documentElement.scrollWidth;
-            $('#content').add($('#content').parents()).each(function(){
-                scrollWidth += this.scrollWidth - this.clientWidth;
-            });
-            if (scrollWidth > clientWidth) {
-                parent.postMessage('PUT height\n\n' + scrollWidth, '*');
-            }
-        }
-    }
-
     if (window.parent != window) {
         document.documentElement.className += " iframe";
         var src = null;
@@ -43,14 +15,5 @@
         }
         $(window).bind('popstate', postSource);
         $(window).bind('load', postSource);
-        var checkNumber = 0;
-        $(window).bind('load resize DOMNodeInserted', function() {
-            var lastCheck = ++checkNumber;
-            setTimeout(function() {
-                if (checkNumber == lastCheck) {
-                    checkWindowSize();
-                }
-            }, 0);
-        });
     }
 })(jQuery);

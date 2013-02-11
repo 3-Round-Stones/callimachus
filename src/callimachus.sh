0974c81b8515def6adf476f21c9681cb3c8bd52c
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2013-02-11 20:57:40 +0000

    Require scripts (except setup) to be run as root if DAEMON_USER defined

--- src/callimachus.sh
+++ src/callimachus.sh
@@ -415,6 +415,11 @@ if [ ! -z "$DAEMON_USER" ] ; then
   fi
 fi
 
+# Make sure only root can run our script
+if [ "$1" != "setup" -a -n "$DAEMON_USER" -a "$(id -u)" != "0" ]; then
+ echo "This script must be run as root" 1>&2
+ exit 4
+fi
 
 #
 # Function that starts the daemon/service

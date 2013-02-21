074d7a4fced38c1424183ade67341f49acae0116
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2013-02-21 20:11:00 +0000

    Treat client i/o errors as warnings in logs

--- src/org/callimachusproject/server/process/Exchange.java
+++ src/org/callimachusproject/server/process/Exchange.java
@@ -358,7 +358,7 @@ public class Exchange implements Cancellable {
 
 		@Override
 		public void failed(Exception ex) {
-			logger.error(ex.toString());
+			logger.warn(ex.toString());
 		}
 
 		@Override

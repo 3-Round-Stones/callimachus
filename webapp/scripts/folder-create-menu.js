bb796b79ce7604a92cc6be72e1bb86a3f20eb858
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2013-04-13 02:58:32 +0000

    Add class rdfs:comment to create title

--- webapp/scripts/folder-create-menu.js
+++ webapp/scripts/folder-create-menu.js
@@ -12,27 +12,30 @@ jQuery(function($) {
                 var menu;
                 var section;
                 $(data.rows).each(function(){
-                    if (menu != this[3]) {
+                    if (menu != this[4]) {
                         if (menu) {
                             ul.append('<li class="divider"></li>');
                             ul.append('<li class="dropdown-submenu dropup"><a tabindex="-1" href="javascript:void(0)">More options</a><ul class="dropdown-menu"></ul></li>')
                             ul = ul.find('ul');
                         }
-                        menu = this[3];
-                        section = this[4];
-                    } else if (section != this[4]) {
+                        menu = this[4];
+                        section = this[5];
+                    } else if (section != this[5]) {
                         if (section) {
                             ul.append('<li class="divider"></li>');
                         }
-                        section = this[4];
+                        section = this[5];
                     }
                     var li = $('<li></li>');
                     var a = $('<a></a>');
                     a.attr('href', this[0]);
                     a.text(this[1]);
                     if (this[2]) {
+                        a.attr('title', this[2]);
+                    }
+                    if (this[3]) {
                         var img = $('<img class="icon"></img>');
-                        img.attr('src', this[2]);
+                        img.attr('src', this[3]);
                         a.prepend(' ');
                         a.prepend(img);
                     }

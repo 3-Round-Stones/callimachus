cbf2a0db94a4134e706abd23841ec5fac57705c8
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2013-04-18 14:53:23 +0000

    Auto adjust ulimit -n if needed

--- src/callimachus.sh
+++ src/callimachus.sh
@@ -383,6 +383,18 @@ if [ ! -e "$JMXRIMPASS" -a -r "$JMXRMI" ] ; then
   chmod 600 "$JMXRIMPASS"
 fi
 
+if [ ! -f "$ULIMIT" ]; then
+  ULIMIT=`command -v ulimit`
+fi
+
+if [ -e "$ULIMIT" ] && [ "$("$ULIMIT" -n)" -lt 4096 ]; then
+  "$ULIMIT" -n 4096
+fi
+
+if [ -e "$ULIMIT" ] && [ "$("$ULIMIT" -n)" -lt 4096 ]; then
+  log_warning_msg "The maximum number of open file descriptors maybe too small"
+fi
+
 if [ ! -z "$DAEMON_USER" ] ; then
   if [ ! -e "$MAIL" ]; then
     touch "$MAIL"

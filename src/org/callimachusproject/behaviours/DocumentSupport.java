0f48c63d33316c351da386b1f55de85d673b8f6a
james@3roundstones.com <james@3roundstones.com@5a417a11-4149-54be-8e14-488cf31dc158>
2013-09-19 14:42:23 +0000

    Cache less.js output more aggressively and never reset it

---
+++ src/org/callimachusproject/behaviours/DocumentSupport.java
@@ -0,0 +1,114 @@
+package org.callimachusproject.behaviours;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+import javax.tools.FileObject;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.callimachusproject.concepts.Activity;
+import org.callimachusproject.io.ChannelUtil;
+import org.callimachusproject.traits.CalliObject;
+import org.openrdf.repository.object.ObjectConnection;
+import org.openrdf.store.blob.BlobObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class DocumentSupport implements CalliObject {
+	private static final Set<String> locks = new HashSet<String>();
+	private final Logger logger = LoggerFactory
+			.getLogger(DocumentSupport.class);
+
+	public String ifStringModifiedSince(String uri, Callable<String> producer)
+			throws Exception {
+		long since = getResourceLastModified(1);
+		BlobObject file = this.getObjectConnection().getBlobObject(uri);
+		if (file.getLastModified() >= since) {
+			return readString(file);
+		} else {
+			lock(uri);
+			try {
+				return produce(file, since, producer);
+			} finally {
+				unlock(uri);
+			}
+		}
+	}
+
+	private String produce(BlobObject file, long since,
+			Callable<String> producer) throws Exception {
+		String text = null;
+		try {
+			ObjectConnection con = this.getCalliRepository().getConnection();
+			try {
+				String uri = file.toUri().toASCIIString();
+				BlobObject blob = con.getBlobObject(uri);
+				if (blob.getLastModified() >= since) {
+					return readString(blob);
+				} else {
+					text = producer.call();
+					OutputStream out = blob.openOutputStream();
+					try {
+						out.write(text.getBytes());
+					} finally {
+						out.close();
+					}
+				}
+			} finally {
+				con.close();
+			}
+		} catch (Exception e) {
+			if (text == null)
+				throw e;
+			logger.error(e.toString(), e);
+		} catch (Error e) {
+			if (text == null)
+				throw e;
+			logger.error(e.toString(), e);
+		}
+		return text;
+	}
+
+	private void lock(String uri) throws InterruptedException {
+		synchronized (locks) {
+			while (!locks.add(uri)) {
+				locks.wait();
+			}
+		}
+	}
+
+	private void unlock(String uri) {
+		synchronized (locks) {
+			locks.remove(uri);
+			locks.notifyAll();
+		}
+	}
+
+	private String readString(FileObject file) throws IOException,
+			UnsupportedEncodingException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		InputStream in = file.openInputStream();
+		try {
+			ChannelUtil.transfer(in, baos);
+		} finally {
+			in.close();
+		}
+		return new String(baos.toByteArray());
+	}
+
+	private long getResourceLastModified(long defaultValue) {
+		Activity activity = this.getProvWasGeneratedBy();
+		if (activity == null)
+			return defaultValue;
+		XMLGregorianCalendar time = activity.getProvEndedAtTime();
+		if (time == null)
+			return defaultValue;
+		return time.toGregorianCalendar().getTimeInMillis();
+	}
+}

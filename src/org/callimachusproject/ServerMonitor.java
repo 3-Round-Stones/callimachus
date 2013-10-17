ee269df9c5bc8ff813bd0ad0dfdfbe63c881479e
James Leigh <james@3roundstones.com>
2013-10-17 12:59:14 -0400

    Handle another shutdown case

--- src/org/callimachusproject/ServerMonitor.java
+++ src/org/callimachusproject/ServerMonitor.java
@@ -36,6 +36,7 @@ import java.util.Set;
 import java.util.TimeZone;
 import java.util.concurrent.ThreadFactory;
 
+import javax.management.InstanceNotFoundException;
 import javax.management.JMX;
 import javax.management.MBeanServerConnection;
 import javax.management.MalformedObjectNameException;
@@ -58,8 +59,8 @@ import org.callimachusproject.management.JVMSummary;
 import org.callimachusproject.management.JVMSummaryMXBean;
 import org.callimachusproject.repository.CalliRepository;
 import org.callimachusproject.repository.CalliRepositoryMXBean;
-import org.callimachusproject.server.WebServerMXBean;
 import org.callimachusproject.server.WebServer;
+import org.callimachusproject.server.WebServerMXBean;
 
 /**
  * Command line tool for monitoring the server.
@@ -209,12 +210,20 @@ public class ServerMonitor {
 				}
 				info("Callimachus server has stopped");
 				return true;
+			} catch (UndeclaredThrowableException e) {
+				if (e.getCause() instanceof InstanceNotFoundException) {
+					// remote MBean has unregistered
+					info("Callimachus server has been destroyed");
+					return true;
+				}
+				throw e;
 			} catch (UnmarshalException e) {
-				if (!(e.getCause() instanceof IOException))
-					throw e;
-				// remote JVM has terminated
-				info("Callimachus server has shutdown");
-				return true;
+				if (e.getCause() instanceof IOException) {
+					// remote JVM has terminated
+					info("Callimachus server has shutdown");
+					return true;
+				}
+				throw e;
 			}
 		} catch (UndeclaredThrowableException e) {
 			throw e.getCause();

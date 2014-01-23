390a282227a0ea4c8c79184f92612ac99e7deeac
James Leigh <james@3roundstones.com>
2014-01-23 09:25:28 -0500

    Fix help and version start flags

--- src/org/callimachusproject/Setup.java
+++ src/org/callimachusproject/Setup.java
@@ -155,20 +155,19 @@ public class Setup {
 	public void init(String[] args) {
 		try {
 			Command line = commands.parse(args);
-			if (line.isParseError()) {
-				if (line.has("help")) {
-					line.printHelp();
-					System.exit(0);
-					return;
-				} else if (line.has("version")) {
-					line.printCommandName();
-					System.exit(0);
-					return;
-				} else {
-					line.printParseError();
-					System.exit(2);
-					return;
-				}
+			if (line.has("help")) {
+				line.printHelp();
+				System.exit(0);
+				return;
+			} else if (line.has("version")) {
+				line.printCommandName();
+				System.exit(0);
+				return;
+			} else 
+				if (line.isParseError()) {
+				line.printParseError();
+				System.exit(2);
+				return;
 			} else {
 				if (line.has("basedir")) {
 					basedir = new File(line.get("basedir"));

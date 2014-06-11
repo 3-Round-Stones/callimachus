26dfedb4e2095da43ddc82afa5fa141ebc0a3859
James Leigh <james@3roundstones.com>
2014-06-11 10:36:00 -0400

    Fix #138 Drop All Support for Diverted links

--- webapp/scripts/resource-iri.js
+++ webapp/scripts/resource-iri.js
@@ -12,74 +12,13 @@ if (!window.calli) {
 
 window.calli.listResourceIRIs = function (text) {
     var set = text ? text.replace(/\s+$/,"").replace(/^\s+/,"").replace(/\s+/g,'\n') : "";
-    return $(set.split('\n')).filter(function() {
-        if (this.length <= 0) {
+    return $(set.split(/[^a-zA-Z0-9\-\._~%!\$\&'\(\)\*\+,;=:\/\?\#\[\]@]+/)).filter(function() {
+        if (!this || this.indexOf('_:') <= 0)
             return false;
-        }
-        if (this.indexOf('>') >= 0 || this.indexOf('<') >= 0) {
-            return false;
-        }
-        if (this.indexOf('}') >= 0 || this.indexOf('{') >= 0) {
-            return false;
-        }
-        if (this.indexOf('\n') >= 0 || this.indexOf('\r') >= 0) {
-            return false;
-        }
-        if (this.indexOf(':') < 0 && this.indexOf('/') < 0 || this.indexOf('_:') >= 0) {
-            return false;
-        }
-        return true;
+        return this.indexOf(':') >= 0 || this.indexOf('/') >= 0;
     }).map(function() {
         var url = this;
-        if (url.indexOf('/diverted;') >= 0) {
-            var uri = url.substring(url.indexOf('/diverted;') + '/diverted;'.length);
-            if (uri.indexOf('?') >= 0) {
-                uri = uri.substring(0, uri.indexOf('?'));
-            }
-            if (uri.indexOf('#') >= 0) {
-                uri = uri.substring(0, uri.indexOf('#'));
-            }
-            return decodeURIComponent(uri);
-        } else if (url.indexOf('/?go=') >= 0) {
-            var uri = url.substring(url.indexOf('/?go=') + '/?go='.length);
-            if (uri.indexOf('&') >= 0) {
-                uri = uri.substring(0, uri.indexOf('&'));
-            }
-            if (uri.indexOf('#') >= 0) {
-                uri = uri.substring(0, uri.indexOf('#'));
-            }
-            uri = decodeURIComponent(uri);
-            if (uri.indexOf('/') == 0) {
-                uri = url.substring(0, url.indexOf('/?go=')) + uri;
-            }
-            return uri;
-        } else if (url.indexOf('/callimachus/view?q=') >= 0) {
-            var uri = url.substring(url.indexOf('/callimachus/view?q=') + '/callimachus/view?q='.length);
-            if (uri.indexOf('&') >= 0) {
-                uri = uri.substring(0, uri.indexOf('&'));
-            }
-            if (uri.indexOf('#') >= 0) {
-                uri = uri.substring(0, uri.indexOf('#'));
-            }
-            uri = decodeURIComponent(uri);
-            if (uri.indexOf('/') == 0) {
-                uri = url.substring(0, url.indexOf('/callimachus/view?q=')) + uri;
-            }
-            return uri;
-        } else if (url.indexOf('/callimachus/go?q=') >= 0) {
-            var uri = url.substring(url.indexOf('/callimachus/go?q=') + '/callimachus/go?q='.length);
-            if (uri.indexOf('&') >= 0) {
-                uri = uri.substring(0, uri.indexOf('&'));
-            }
-            if (uri.indexOf('#') >= 0) {
-                uri = uri.substring(0, uri.indexOf('#'));
-            }
-            uri = decodeURIComponent(uri);
-            if (uri.indexOf('/') == 0) {
-                uri = url.substring(0, url.indexOf('/callimachus/go?q=')) + uri;
-            }
-            return uri;
-        } else if (url.indexOf('?view') >= 0) {
+        if (url.indexOf('?view') >= 0) {
             return url.substring(0, url.indexOf('?view'));
         }
         return url.substring(0);

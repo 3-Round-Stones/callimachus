8c101b9ef6585a62ae6fe8fa8ec8a443f0ccecf5
David Wood <david@3roundstones.com>
2014-06-12 18:56:55 -0400

    Backed out the change that would exit with error if the setup script was not run first because it would break CE remote configuration.

--- src/callimachus.sh
+++ src/callimachus.sh
@@ -551,10 +551,6 @@ daemon_run()
 #
 do_start()
 {
-  if [ -z "$ORIGIN" ] ; then
-    log_failure_msg "No Web origin configured. Have you run callimachus-setup.sh first?"
-    return 6
-  fi
   LSOF="$(which lsof 2>/dev/null)"
   LSOF_OPTS="$(echo $PORT |perl -pe 's/(^|\s)(\S)/ -i :$2/g' 2>/dev/null) $(echo $SSLPORT |perl -pe 's/(^|\s)(\S)/ -i :$2/g' 2>/dev/null)"
   if [ -n "$LSOF" ] && [ -n "$PORT" -o -n "$SSLPORT" ] && "$LSOF" $LSOF_OPTS |grep "LISTEN" ; then

47e4ffaef12fda94a6b8251d3860fc0e0e481bfb
James Leigh <james@3roundstones.com>
2014-06-16 10:37:38 -0400

    Quietly log EOFExceptions from request body

--- src/org/callimachusproject/xml/XdmNodeFactory.java
+++ src/org/callimachusproject/xml/XdmNodeFactory.java
@@ -42,7 +42,7 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
 public class XdmNodeFactory implements EntityResolver, URIResolver, ModuleURIResolver {
-	private static final String XML_MEDIA = "application/xml, application/xslt+xml, text/xml, text/xsl";
+	private static final String XML_MEDIA = "application/xml, application/xslt+xml, application/xproc+xml, text/xml, text/xsl";
 	private static final String XQUERY_MEDIA = "application/xquery, " + XML_MEDIA;
 	private final Processor processor;
 	private final InputSourceResolver xmlResolver;

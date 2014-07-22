eaf4c098fc1b4e79f4c38d48d36ad2a62097d58d
James Leigh <james@3roundstones.com>
2014-07-22 13:59:11 -0400

    Replace file upload form with calli.submitForm

--- webapp/scripts/submit-form-data.js
+++ webapp/scripts/submit-form-data.js
@@ -4,22 +4,83 @@
    Licensed under the Apache License, Version 2.0, http://www.apache.org/licenses/LICENSE-2.0
 */
 
+(function($){
+
+var calli = window.calli = window.calli || {};
+
+calli.submitForm = function(event) {
+    event = calli.fixEvent(event);
+    event.preventDefault();
+    var form = $(event.target).closest('form')[0];
+    var finalTarget = form.target;
+    return calli.postForm(form).then(function(redirect){
+        if (finalTarget && window.frames[finalTarget]) {
+            window.frames[finalTarget].location.href = redirect;
+        } else {
+            if (window.parent != window && parent.postMessage) {
+                parent.postMessage('PUT src\n\n' + redirect, '*');
+            }
+            window.location.href = redirect;
+        }
+    });
+};
+
+calli.postForm = function(form) {
+    return calli.resolve($(form)[0]).then(function(form){
+        var iframe = document.createElement("iframe");
+        iframe.name = newIframeName();
+        iframe.style.display = "none";
+        $('body').append(iframe);
+        return new Promise(function(resolve){
+            var finalTarget = form.target;
+            iframe.bind('load', function(event){
+                var doc = event.target.contentWindow.document;
+                if (doc.URL != "about:blank") {
+                    resolve($(doc).text());
+                    $(iframe).remove();
+                }
+            });
+            form.target = iframe.name;
+            form.submit();
+            form.target = finalTarget;
+        }).then(function(redirect){
+            if (redirect && redirect.indexOf('http') === 0) {
+                return redirect;
+            } else {
+                var doc = this.contentWindow.document;
+                var h1 = $(doc).find('h1').clone();
+                var frag = document.createDocumentFragment();
+                h1.contents().each(function() {
+                    frag.appendChild(this);
+                });
+                var pre = $(doc).find('pre').text();
+                return calli.reject({message: frag, stack: pre});
+            }
+        });
+    });
+};
+
 jQuery(function($){
+    $('form[method="POST"][enctype="multipart/form-data"],form[method="POST"][enctype="application/x-www-form-urlencoded"]').submit(function(event) {
+        var form = $(this);
+        var resource = form.attr('about') || form.attr('resource');
+        if (!resource || resource.indexOf(':') < 0 && resource.indexOf('/') !== 0 && resource.indexOf('?') !== 0)
+            return true; // resource attribute not set yet
+        if (!this.target || this.target.indexOf('iframe-redirect-') !== 0) {
+            var iname = newIframeName();
+            createIframeRedirect(iname, this.target, resource, event);
+            this.target = iname;
+        }
+        return true;
+    });
+});
 
 var iframe_counter = 0;
-$('form[method="POST"][enctype="multipart/form-data"],form[method="POST"][enctype="application/x-www-form-urlencoded"]').submit(function(event) {
-    var form = $(this);
-    var resource = form.attr('about') || form.attr('resource');
-    if (!resource || resource.indexOf(':') < 0 && resource.indexOf('/') !== 0 && resource.indexOf('?') !== 0)
-        return true; // resource attribute not set yet
-    if (!this.target || this.target.indexOf('iframe-redirect-') !== 0) {
-        var iname = null;
-        while (window.frames[iname = 'iframe-redirect-' + (++iframe_counter)]);
-        createIframeRedirect(iname, this.target, resource, event);
-        this.target = iname;
-    }
-    return true;
-});
+function newIframeName() {
+    var iname = null;
+    while (window.frames[iname = 'iframe-redirect-' + (++iframe_counter)]);
+    return iname;
+}
 
 function createIframeRedirect(iname, finalTarget, resource, cause) {
     var iframe = $('<iframe></iframe>');
@@ -60,4 +121,4 @@ function createIframeRedirect(iname, finalTarget, resource, cause) {
     return iframe;
 }
 
-});
+})(jQuery);

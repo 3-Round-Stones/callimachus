1efafa5ee54f3747b1d931e231f02d28c7479218
James Leigh <james@3roundstones.com>
2014-07-29 10:31:21 -0400

    Move calli.submitForm to its own file

--- webapp/scripts/submit-form-data.js
+++ webapp/scripts/submit-form-data.js
@@ -6,69 +6,6 @@
 
 (function($){
 
-var calli = window.calli || (window.calli={});
-
-calli.submitForm = function(event) {
-    event = calli.fixEvent(event);
-    event.preventDefault();
-    var form = $(event.target).closest('form')[0];
-    var finalTarget = form.target;
-    var btn = $(form).find('button[type="submit"]');
-    btn.button('loading');
-    return calli.postForm(form).then(function(redirect){
-        return redirect && redirect + "?view";
-    }).then(function(redirect){
-        if (finalTarget && window.frames[finalTarget]) {
-            window.frames[finalTarget].location.href = redirect;
-        } else {
-            if (window.parent != window && parent.postMessage) {
-                parent.postMessage('PUT src\n\n' + redirect, '*');
-            }
-            window.location.href = redirect;
-        }
-    }, function(error) {
-        btn.button('reset');
-        return calli.error(error);
-    });
-};
-
-calli.postForm = function(form) {
-    return calli.resolve($(form)[0]).then(function(form){
-        var iframe = document.createElement("iframe");
-        iframe.name = newIframeName();
-        iframe.style.display = "none";
-        $('body').append(iframe);
-        return new Promise(function(resolve){
-            var finalTarget = form.target;
-            $(iframe).bind('load', function(event){
-                var doc = iframe.contentDocument;
-                if (doc.URL != "about:blank") {
-                    resolve($(doc).text());
-                }
-            });
-            form.target = iframe.name;
-            form.submit();
-            form.target = finalTarget;
-        }).then(function(redirect){
-            if (redirect && redirect.indexOf('http') === 0) {
-                $(iframe).remove();
-                return redirect;
-            } else {
-                var doc = iframe.contentDocument;
-                var h1 = $(doc).find('h1').clone();
-                var frag = document.createDocumentFragment();
-                h1.contents().each(function() {
-                    frag.appendChild(this);
-                });
-                var pre = $(doc).find('pre').text();
-                $(iframe).remove();
-                if (!pre) return calli.reject(frag);
-                return calli.reject({message: frag, stack: pre});
-            }
-        });
-    });
-};
-
 jQuery(function($){
     $('form[method="POST"][enctype="multipart/form-data"],form[method="POST"][enctype="application/x-www-form-urlencoded"]').submit(function(event) {
         var form = $(this);
